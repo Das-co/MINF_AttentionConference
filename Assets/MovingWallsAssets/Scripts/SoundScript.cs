a5422c27d2bb72ffa1051bc248dedc7716061a40
unknown <brian.walter@haw-hamburg.de>
2018-04-13 22:29:56 +0200

    Sound maybe

---
+++ Assets/MovingWallsAssets/Scripts/SoundScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour {
+
+    public AudioSource bling;
+    private bool soundState = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (soundState == null)
+        {
+            return;
+        }
+
+        if(soundState == true)
+        {
+            bling.Play();
+            soundState = false;
+        }
+
+		
+	}
+    [PunRPC]
+    public void SendSound()
+    {
+        soundState = true;
+       
+    }
+}
